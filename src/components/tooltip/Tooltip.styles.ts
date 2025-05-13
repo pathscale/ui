4f9557d165a66ba9a3de5fd290c250c15f1988ef
Diego Martinez <diegoalejandromartinezrivas@gmail.com>
2025-05-13 19:51:15 -0400

    feat: implement tooltip arrow and adjust positioning for all sides

--- src/components/tooltip/Tooltip.styles.ts
+++ src/components/tooltip/Tooltip.styles.ts
@@ -6,6 +6,7 @@ export const tooltipVariants = cva(
     "opacity-0 group-hover:opacity-100",
     "pointer-events-none transition-opacity duration-200",
     "max-w-xs break-words",
+    "after:content-[''] after:absolute after:w-2 after:h-2 after:rotate-45 after:bg-inherit",
   ].join(" "),
   {
     variants: {
@@ -39,10 +40,15 @@ export const tooltipVariants = cva(
         false: "",
       },
       position: {
-        top: "bottom-full left-1/2 -translate-x-1/2 mb-2",
-        bottom: "top-full left-1/2 -translate-x-1/2 mt-2",
-        left: "right-full top-1/2 -translate-y-1/2 mr-2",
-        right: "left-full top-1/2 -translate-y-1/2 ml-2",
+        top: "bottom-full left-1/2 -translate-x-1/2 mb-1 after:top-full after:left-1/2 after:-translate-x-1/2 after:-mt-1",
+
+        bottom:
+          "top-full left-1/2 -translate-x-1/2 mt-1 after:bottom-full after:left-1/2 after:-translate-x-1/2 after:-mb-1",
+
+        left: "right-full top-1/2 -translate-y-1/2 mr-1 after:left-full after:top-1/2 after:-translate-y-1/2 after:-ml-1",
+
+        right:
+          "left-full top-1/2 -translate-y-1/2 ml-1 after:right-full after:top-1/2 after:-translate-y-1/2 after:-mr-1",
       },
     },
     defaultVariants: {
