a7b0605eea151a44772abbc4570c53023e3a07d0
AbsoluteC2H6O <alfredofp63@gmail.com>
2025-05-14 21:00:49 -0400

    feature(navbar): fixed Tag component and replace by Dinamyc

--- src/components/navbar/NavbarItem.tsx
+++ src/components/navbar/NavbarItem.tsx
@@ -5,6 +5,7 @@ import {
 } from "solid-js";
 import { classes, type VariantProps, type ClassProps } from "@src/lib/style";
 import { navbarItemStyles } from "./Navbar.styles";
+import { Dynamic } from "solid-js/web";
 
 export type NavbarItemProps = {
     active?: boolean;
@@ -26,12 +27,15 @@ const NavbarItem: Component<NavbarItemProps> = (props) => {
         Object.keys(navbarItemStyles.variantKeys ?? {}) as any
     );
 
+    const Tag = local.tag === "a" || local.href || local.to ? "a" : "div";
+
     return (
-        <div
+        <Dynamic
+            component={Tag}
             class={classes(
                 navbarItemStyles({
                     active: !!local.active,
-                    tag: local.tag ?? "div",
+                    tag: local.tag ?? (Tag === "a" ? "a" : "div"),
                 }),
                 local.class,
                 local.className
@@ -40,7 +44,7 @@ const NavbarItem: Component<NavbarItemProps> = (props) => {
             {...other}
         >
             {local.children}
-        </div>
+        </Dynamic>
     );
 };
 
