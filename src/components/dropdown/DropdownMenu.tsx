f283da7e5cfc9c1e25affab78bb98cad2cd72de1
AbsoluteC2H6O <alfredofp63@gmail.com>
2025-05-24 02:11:03 -0400

    build(dropdown): migrate dropdown component

--- src/components/dropdown/DropdownMenu.tsx
+++ src/components/dropdown/DropdownMenu.tsx
@@ -1,16 +1,40 @@
-import { Show, useContext, type Component, type JSX } from "solid-js";
-import { DropdownContext } from "./DropdownContext";
-import { dropdownMenuClass } from "./Dropdown.styles";
+import { type JSX, splitProps } from "solid-js";
+import { twMerge } from "tailwind-merge";
 
-const DropdownMenu: Component<{ children: JSX.Element }> = (props) => {
-  const context = useContext(DropdownContext);
+import type { IComponentBaseProps } from "../types";
+
+export type DropdownMenuProps = JSX.HTMLAttributes<HTMLUListElement> &
+  IComponentBaseProps & {
+    class?: string;
+    className?: string;
+    style?: JSX.CSSProperties;
+    "data-theme"?: string;
+  };
+
+const DropdownMenu = (props: DropdownMenuProps): JSX.Element => {
+  const [local, others] = splitProps(props, [
+    "class",
+    "className",
+    "data-theme",
+    "style",
+  ]);
+
+  const classes = () =>
+    twMerge(
+      "dropdown-content menu p-2 shadow bg-base-100 rounded-box",
+      local.class,
+      local.className
+    );
 
   return (
-    <Show when={context?.open?.()}>
-      <div class={dropdownMenuClass({ position: context?.position })}>
-        {props.children}
-      </div>
-    </Show>
+    <ul
+      {...others}
+      tabindex={0}
+      data-theme={local["data-theme"]}
+      class={classes()}
+      style={local.style}
+      role="menu"
+    />
   );
 };
 
---
+++ src/components/dropdown/DropdownToggle.tsx
@@ -0,0 +1,57 @@
+import { type JSX, splitProps, Show } from "solid-js";
+import { twMerge } from "tailwind-merge";
+
+import type {
+  ComponentColor,
+  ComponentSize,
+  IComponentBaseProps,
+} from "../types";
+
+import Button from "../Button/Button";
+import type { ButtonProps } from "../Button/Button";
+
+export type DropdownToggleProps = JSX.LabelHTMLAttributes<HTMLLabelElement> &
+  IComponentBaseProps & {
+    color?: ComponentColor;
+    size?: ComponentSize;
+    button?: boolean;
+    disabled?: boolean;
+  };
+
+const DropdownToggle = (props: DropdownToggleProps): JSX.Element => {
+  const [local, rest] = splitProps(props, [
+    "children",
+    "color",
+    "size",
+    "button",
+    "dataTheme",
+    "class",
+    "className",
+    "disabled",
+  ]);
+
+  const classAttr = () => twMerge(local.class, local.className);
+
+  return (
+    <label
+      tabindex={0}
+      class={classAttr()}
+      {...rest}
+      data-theme={local.dataTheme}
+    >
+      <Show when={local.button} fallback={local.children}>
+        <Button
+          type="button"
+          color={local.color}
+          size={local.size}
+          dataTheme={local.dataTheme}
+          disabled={local.disabled}
+        >
+          {local.children}
+        </Button>
+      </Show>
+    </label>
+  );
+};
+
+export default DropdownToggle;
