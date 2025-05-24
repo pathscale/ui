f7c6d445dfe5df6fd74572e5c00f161a6d5e08e8
Diego Martinez <diegoalejandromartinezrivas@gmail.com>
2025-05-24 01:43:15 -0400

    Add daisyui badge (#34)
    * feat: add daisyui badge
    
    * fix: update Tag export to Badge to match renamed component
    
    ---------
    
    Co-authored-by: Skaptox <oalbornoz08@gmail.com>

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,15 +1,4 @@
-import SelectShowcase from "../../src/components/select/SelectShowcase";
-import CheckboxShowcase from "../../src/components/checkbox/CheckboxShowcase";
-
-import { createSignal, onMount } from "solid-js";
-
-import ButtonShowcase from "../../src/components/button/ButtonShowcase";
-import InputShowcase from "../../src/components/input/InputShowcase";
-
-const NAV_ITEMS = [
-  { id: "button", label: "Button" },
-  { id: "input", label: "Input" },
-];
+import BadgeShowcase from "../../src/components/badge/BadgeShowcase";
 
 
 export default function App() {
@@ -32,7 +21,7 @@ export default function App() {
 
   return (
     <main class="min-h-screen">
-      <SelectShowcase />
+      <BadgeShowcase />
     </main>
   );
 }
