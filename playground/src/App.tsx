b3fecee9bb3ae71e5ba2360d4120a9d15ac056d9
Diego Martinez <diegoalejandromartinezrivas@gmail.com>
2025-05-24 01:48:07 -0400

    Add daisyui pagination (#38)
    * feat: add daisyui tooltip
    
    * feat: add daisyui tooltip
    
    ---------
    
    Co-authored-by: Skaptox <oalbornoz08@gmail.com>

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,6 +1,5 @@
-import TooltipShowcase from "../../src/components/tooltip/TooltipShowcase";
+import PaginationShowcase from "../../src/components/pagination/PaginationShowcase";
 
-import BreadcrumbShowcase from "../../src/components/breadcrumbs/BreadcrumbShowcase";
 
 export default function App() {
   const [theme, setTheme] = createSignal<"light" | "dark">("light");
@@ -22,7 +21,7 @@ export default function App() {
 
   return (
     <main class="min-h-screen">
-      <TooltipShowcase />
+      <PaginationShowcase />
     </main>
   );
 }
