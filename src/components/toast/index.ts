ac9d25c468a88e33f8e592a507c2641319c17bd8
Diego Martinez <diegoalejandromartinezrivas@gmail.com>
2025-05-26 05:31:04 -0400

    Add daisyui toast (#48)
    * feat: add daisyui toast
    
    * docs: mark Toast as completed in README

--- src/components/toast/index.ts
+++ src/components/toast/index.ts
@@ -1,6 +1,1 @@
-import { createToastApi } from "./toast-api";
-import Toaster from "./Toaster";
-
-export const toast = createToastApi();
-export { Toaster };
-export default toast;
+export { default } from "./Toast";
