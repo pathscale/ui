bf9cd0548ae0ba21d9db6fd8904d907f7803b85d
aianovnics <ayanovnix@gmail.com>
2025-05-27 22:43:49 +0500

    feat: indicator component

---
+++ src/components/indicator/index.ts
@@ -0,0 +1,4 @@
+export { default, Indicator, IndicatorItem } from "./Indicator";
+export type { IndicatorItemProps, IndicatorProps } from "./Indicator";
+export { default as IndicatorShowcase } from "./IndicatorShowcase";
+
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,50 @@
-export { default as Avatar } from "./components/avatar";
-export { default as Alert } from "./components/alert";
-export { default as Button } from "./components/button";
-export { default as Diff } from "./components/diff";
-export { default as Input } from "./components/input";
-export { default as Link } from "./components/link";
-export { default as Textarea } from "./components/textarea";
-export { default as Progress } from "./components/progress";
-export { default as Stats } from "./components/stats";
-export { default as FileInput } from "./components/fileinput";
 export { default as Accordion } from "./components/accordion";
-export { default as Pagination } from "./components/pagination";
-export { default as Tabs } from "./components/tabs";
-export { default as Tooltip } from "./components/tooltip";
-export { default as Badge } from "./components/badge";
-export { default as Select } from "./components/select";
-export { default as Toggle } from "./components/toggle";
-export { default as Checkbox } from "./components/checkbox";
-export { Breadcrumbs, BreadcrumbsItem } from "./components/breadcrumbs";
+export { default as Alert } from "./components/alert";
 export { default as Autocomplete } from "./components/autocomplete";
-export { default as Dropdown } from "./components/dropdown";
-export { default as Field } from "./components/field";
-export { Menu } from "./components/menu";
-export { default as Modal } from "./components/modal";
-export { Navbar, NavbarItem, NavbarDropdown } from "./components/navbar";
-export { Polymorphic, PolymorphicButton } from "./components/polymorphic";
-export { default as Steps } from "./components/steps";
-export { default as Swap } from "./components/swap";
-export { default as Table } from "./components/table";
-export {
-  Timeline,
-  TimelineItem,
-  TimelineStart,
-  TimelineMiddle,
-  TimelineEnd,
-} from "./components/timeline";
-export { default as Toast } from "./components/toast";
-export { default as Mask } from "./components/mask";
-export { default as Join } from "./components/join";
-export { default as Loading } from "./components/loading";
+export { default as Avatar } from "./components/avatar";
+export { default as Badge } from "./components/badge";
+export { Breadcrumbs, BreadcrumbsItem } from "./components/breadcrumbs";
+export { default as Button } from "./components/button";
+export { default as Card } from "./components/card";
+export { default as Checkbox } from "./components/checkbox";
 export {
   Collapse,
   CollapseContent,
   CollapseDetails,
   CollapseTitle,
-  Summary,
+  Summary
 } from "./components/collapse";
 export { default as Countdown } from "./components/countdown";
-export { default as Card } from "./components/card";
-export { default as Range } from "./components/range";
-export { default as Radio } from "./components/radio";
+export { default as Diff } from "./components/diff";
+export { default as Dropdown } from "./components/dropdown";
+export { default as Field } from "./components/field";
+export { default as FileInput } from "./components/fileinput";
+export { default as Indicator } from "./components/indicator";
+export { default as Input } from "./components/input";
+export { default as Join } from "./components/join";
 export { default as Kbd } from "./components/kbd";
+export { default as Link } from "./components/link";
+export { default as Loading } from "./components/loading";
+export { default as Mask } from "./components/mask";
+export { Menu } from "./components/menu";
+export { default as Modal } from "./components/modal";
+export { Navbar, NavbarDropdown, NavbarItem } from "./components/navbar";
+export { default as Pagination } from "./components/pagination";
+export { Polymorphic, PolymorphicButton } from "./components/polymorphic";
+export { default as Progress } from "./components/progress";
+export { default as Radio } from "./components/radio";
+export { default as Range } from "./components/range";
+export { default as Select } from "./components/select";
 export { default as Skeleton } from "./components/skeleton";
+export { default as Stats } from "./components/stats";
+export { default as Steps } from "./components/steps";
+export { default as Swap } from "./components/swap";
+export { default as Table } from "./components/table";
+export { default as Tabs } from "./components/tabs";
+export { default as Textarea } from "./components/textarea";
+export {
+  Timeline, TimelineEnd, TimelineItem, TimelineMiddle, TimelineStart
+} from "./components/timeline";
+export { default as Toast } from "./components/toast";
+export { default as Toggle } from "./components/toggle";
+export { default as Tooltip } from "./components/tooltip";
