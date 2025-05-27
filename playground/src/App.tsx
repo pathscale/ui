f29ed86aaeee105c1c0b7c677b2a0c81d3a89094
aianovnics <ayanovnix@gmail.com>
2025-05-27 22:52:10 +0500

    feat: component indicator

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,5 +1,6 @@
 import { createSignal, onMount } from "solid-js";
-import NavbarShowcase from "../../src/components/navbar/NavbarShowcase";
+
+import IndicatorShowcase from "../../src/components/indicator/IndicatorShowcase";
 
 export default function App() {
   const [theme, setTheme] = createSignal<"light" | "dark">("light");
@@ -31,7 +32,7 @@ export default function App() {
       </aside>
 
       <main class="flex-1 p-8 space-y-16 scroll-smooth">
-        <NavbarShowcase />
+        <IndicatorShowcase />
       </main>
     </div>
   );
