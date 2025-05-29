4e5951f90fc02dd81e2f0ad1d92ab444974e48c5
alexlyaps <a34270622@gmail.com>
2025-05-29 14:27:07 +0200

    fix: resize-textarea

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,6 +1,6 @@
 import { createSignal, onMount } from "solid-js";
 
-import ShowcaseSectionShowcase from "../../src/components/showcase-section/ShowcaseSectionShowcase";
+import TextareaShowcase from "../../src/components/textarea/TextareaShowcase";
 
 export default function App() {
   const [theme, setTheme] = createSignal<"light" | "dark">("light");
@@ -32,7 +32,7 @@ export default function App() {
       </aside>
 
       <main class="flex-1 p-8 space-y-16 scroll-smooth">
-        <ShowcaseSectionShowcase />
+        <TextareaShowcase />
       </main>
     </div>
   );
