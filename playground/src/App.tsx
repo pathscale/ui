fd82a71e31c57cb9636fa6e890f5ec12f60d10eb
alexlyaps <a34270622@gmail.com>
2025-05-29 15:32:00 +0200

    fix/chatbubble-position

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,6 +1,6 @@
 import { createSignal, onMount } from "solid-js";
 
-import ShowcaseSectionShowcase from "../../src/components/showcase-section/ShowcaseSectionShowcase";
+import ChatBubbleShowcase from "../../src/components/chatbubble/ChatBubbleShowcase";
 
 export default function App() {
   const [theme, setTheme] = createSignal<"light" | "dark">("light");
@@ -32,7 +32,7 @@ export default function App() {
       </aside>
 
       <main class="flex-1 p-8 space-y-16 scroll-smooth">
-        <ShowcaseSectionShowcase />
+        <ChatBubbleShowcase />
       </main>
     </div>
   );
