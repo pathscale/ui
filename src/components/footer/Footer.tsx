855cebb600398279f6f50d4e3709ae62ac287f6f
nikita <shealtov@gmail.com>
2025-05-29 17:12:44 +0300

    refactor: type replace for interface props, classes function added

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,62 +1,61 @@
 import { ParentComponent, splitProps } from "solid-js";
 import { twMerge } from "tailwind-merge";
-
+import clsx from "clsx";
 import type { IComponentBaseProps } from "../types";
 
-export interface FooterProps extends IComponentBaseProps {
-	center?: boolean;
-	horizontal?: boolean;
-	vertical?: boolean;
-}
+export type FooterProps = IComponentBaseProps & {
+  center?: boolean;
+  horizontal?: boolean;
+  vertical?: boolean;
+};
 
 const Footer: ParentComponent<FooterProps> = (props) => {
-	const [local, others] = splitProps(props, [
-		"children",
-		"class",
-		"className",
-		"dataTheme",
-		"center",
-		"horizontal",
-		"vertical"
-	]);
+  const [local, others] = splitProps(props, [
+    "children",
+    "class",
+    "className",
+    "dataTheme",
+    "center",
+    "horizontal",
+    "vertical",
+  ]);
 
-	return (
-		<footer
-			{...others}
-			data-theme={local.dataTheme}
-			class={twMerge(
-				"footer",
-				local.center && "footer-center",
-				local.horizontal && "footer-horizontal",
-				local.vertical && "footer-vertical",
-				local.class,
-				local.className
-			)}
-		>
-			{local.children}
-		</footer>
-	);
+  const classes = () =>
+    twMerge(
+      "footer",
+      local.class,
+      local.className,
+      clsx({
+        "footer-center": local.center,
+        "footer-horizontal": local.horizontal,
+        "footer-vertical": local.vertical,
+      })
+    );
+
+  return (
+    <footer {...others} data-theme={local.dataTheme} class={classes()}>
+      {local.children}
+    </footer>
+  );
 };
 
-export interface FooterTitleProps extends IComponentBaseProps { }
+export interface FooterTitleProps extends IComponentBaseProps {}
 
 export const FooterTitle: ParentComponent<FooterTitleProps> = (props) => {
-	const [local, others] = splitProps(props, [
-		"children",
-		"class",
-		"className",
-		"dataTheme"
-	]);
+  const [local, others] = splitProps(props, [
+    "children",
+    "class",
+    "className",
+    "dataTheme",
+  ]);
 
-	return (
-		<h6
-			{...others}
-			data-theme={local.dataTheme}
-			class={twMerge("footer-title", local.class, local.className)}
-		>
-			{local.children}
-		</h6>
-	);
+  const classes = () => twMerge("footer-title", local.class, local.className);
+
+  return (
+    <h6 {...others} data-theme={local.dataTheme} class={classes()}>
+      {local.children}
+    </h6>
+  );
 };
 
-export default Footer; 
+export default Footer;
