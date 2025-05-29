5970b48b6abd2b8fbae278c09dc9ba9a3ca2a75d
nikita <shealtov@gmail.com>
2025-05-29 17:46:41 +0300

    fix: index

--- src/components/footer/index.ts
+++ src/components/footer/index.ts
@@ -1,3 +1,3 @@
-export { default as Footer } from "./Footer";
+export { default } from "./Footer";
 export type { FooterProps } from "./Footer";
 export type { FooterTitleProps } from "./FooterTitle";
