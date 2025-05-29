a74abee78a8797a40bf068256dfa37bbcac142d4
alexlyaps <a34270622@gmail.com>
2025-05-29 11:30:55 +0200

    feat: leftIcon, rightIcon for Input

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,6 +1,6 @@
 import { createSignal, onMount } from "solid-js";
 
-import SidenavShowcase from "../../src/components/sidenav/SidenavShowcase";
+import InputShowcase from "../../src/components/input/InputShowcase";
 
 export default function App() {
   const [theme, setTheme] = createSignal<"light" | "dark">("light");
@@ -32,7 +32,7 @@ export default function App() {
       </aside>
 
       <main class="flex-1 p-8 space-y-16 scroll-smooth">
-        <SidenavShowcase />
+        <InputShowcase />
       </main>
     </div>
   );
