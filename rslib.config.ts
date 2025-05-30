819f2bbd61ffe9fe600368c1f167ee6efc7464d8
aianov <122548754+aianov@users.noreply.github.com>
2025-05-30 20:59:47 +0500

    feat: rslib local iconify plugin (#123)

--- rslib.config.ts
+++ rslib.config.ts
@@ -1,6 +1,7 @@
 import { pluginBabel } from "@rsbuild/plugin-babel";
 import { pluginSolid } from "@rsbuild/plugin-solid";
 import { defineConfig } from "@rslib/core";
+import { pluginIconify } from "rsbuild-plugin-iconify";
 
 export default defineConfig({
   source: {
@@ -28,5 +29,10 @@ export default defineConfig({
       include: /\.(?:jsx|tsx)$/,
     }),
     pluginSolid(),
+    pluginIconify({
+      targetDir: 'src/styles/icons',
+      includeSets: ['mdi-light', 'material-symbols'],
+      maxIconsPerSet: 1000,
+    }),
   ],
 });
