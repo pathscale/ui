6beec30da3bb9df00b352df75974fea80aec0290
nikita <93532590+fly0utWest@users.noreply.github.com>
2025-05-30 08:58:07 +0300

    feat: add window mockup component (#109)

---
+++ src/components/windowmockup/index.ts
@@ -0,0 +1,1 @@
+export { default, type WindowMockupProps } from "./WindowMockup";
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,14 @@ export { default as Autocomplete } from "./components/autocomplete";
 export { default as Avatar } from "./components/avatar";
 export { default as Background } from "./components/background";
 export { default as Badge } from "./components/badge";
+export {
+  default as PhoneMockup,
+  type PhoneMockupProps,
+} from "./components/phonemockup";
+export {
+  default as WindowMockup,
+  type WindowMockupProps,
+} from "./components/windowmockup";
 export { Breadcrumbs, BreadcrumbsItem } from "./components/breadcrumbs";
 export { default as Button } from "./components/button";
 export { default as Card } from "./components/card";
