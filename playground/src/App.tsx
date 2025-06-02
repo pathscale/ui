874713214ec7aaf575daed516751652b9be74acf
nikita <shealtov@gmail.com>
2025-06-02 15:51:22 +0300

    revert playground App

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,14 +1,11 @@
 import Background from "../../src/components/background";
 import Flex from "../../src/components/flex/Flex";
-import SidenavShowcase from "../../src/components/sidenav/SidenavShowcase";
 
 export default function App() {
   return (
     <Background>
       <Flex class="min-h-screen">
-        <main class="flex-1 p-8 space-y-16 scroll-smooth">
-          <SidenavShowcase />
-        </main>
+        <main class="flex-1 p-8 space-y-16 scroll-smooth"></main>
       </Flex>
     </Background>
   );
