943370b0f7521d8aba5571e8ca4add65af8fbe60
nikita <shealtov@gmail.com>
2025-06-05 12:10:03 +0300

    feat: add aria for carousel

--- src/components/carousel/CarouselItem.tsx
+++ src/components/carousel/CarouselItem.tsx
@@ -4,15 +4,23 @@ import { twMerge } from "tailwind-merge";
 export type CarouselItemProps = JSX.HTMLAttributes<HTMLDivElement>;
 
 const CarouselItem = (props: ParentProps<CarouselItemProps>) => {
-	const [local, rest] = splitProps(props, ["class", "children"]);
+  const [local, rest] = splitProps(props, ["class", "children"]);
 
-	const classes = twMerge("carousel-item", local.class);
+  const classes = twMerge("carousel-item", local.class);
 
-	return (
-		<div {...rest} class={classes}>
-			{local.children}
-		</div>
-	);
+  const role = "group";
+  const ariaRoleDescription = "slide";
+
+  return (
+    <div
+      {...rest}
+      class={classes}
+      role={role}
+      aria-roledescription={ariaRoleDescription}
+    >
+      {local.children}
+    </div>
+  );
 };
 
-export default CarouselItem; 
+export default CarouselItem;
