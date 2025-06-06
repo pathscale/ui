ad1443fd1532945a57cda88aad78bd44dec67933
nikita <shealtov@gmail.com>
2025-06-06 11:10:30 +0300

    remove showcase from playground's App

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,11 +1,9 @@
 import Background from "../../src/components/background";
 import Flex from "../../src/components/flex/Flex";
-import SidenavDebugShowcase from "../../src/components/sidenav/SidenavDebugShowcase";
 
 export default function App() {
   return (
     <Background>
-      <SidenavDebugShowcase />
       <Flex class="min-h-screen">
         <main class="flex-1 p-8 space-y-16 scroll-smooth"></main>
       </Flex>
