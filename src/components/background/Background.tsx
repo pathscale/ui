22d5545b77fe3d94aeb25c1cab3d3bc8bd4e4f51
nikita <shealtov@gmail.com>
2025-06-23 17:47:15 +0300

    refactor: edit background structure that complies with others component

--- src/components/background/Background.tsx
+++ src/components/background/Background.tsx
@@ -1,19 +1,20 @@
-import type { JSX } from "solid-js";
-import clsx from "clsx";
+import type { Component, JSX } from "solid-js";
 import { twMerge } from "tailwind-merge";
 import type { IComponentBaseProps } from "../types";
+import { splitProps } from "solid-js";
+import { createMemo } from "solid-js";
 
 export type BackgroundProps = IComponentBaseProps &
   JSX.HTMLAttributes<HTMLDivElement>;
 
-const Background = (props: BackgroundProps) => {
-  const { class: className, className: classNameAlt, ...rest } = props;
-  return (
-    <div
-      class={twMerge("bg-base-100 min-h-screen", clsx(className, classNameAlt))}
-      {...rest}
-    />
+const Background: Component<BackgroundProps> = (props) => {
+  const [local, others] = splitProps(props, ["class", "className"]);
+
+  const classes = createMemo(() =>
+    twMerge("bg-base-100 min-h-screen", local.class, local.className)
   );
+
+  return <div class={classes()} {...others} />;
 };
 
 export default Background;
