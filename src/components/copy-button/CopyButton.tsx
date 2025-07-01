de4b6a5663ed42469a78dfcdff9fbcfe56610e2b
nikita <shealtov@gmail.com>
2025-07-01 15:17:27 +0300

    feat: add possibility to add component as a reaction on copy event in copybutton

--- src/components/copy-button/CopyButton.tsx
+++ src/components/copy-button/CopyButton.tsx
@@ -4,49 +4,60 @@ import { twMerge } from "tailwind-merge";
 import type { IComponentBaseProps } from "../types";
 
 export interface CopyButtonProps extends IComponentBaseProps {
-	text: string;
-	title?: string;
-	onCopy?: () => void;
-	children?: JSX.Element;
+  text: string;
+  title?: string;
+  onCopy?: () => void;
+  children?: JSX.Element;
+  copiedToken?: string | Component;
 }
 
 const CopyButton: Component<CopyButtonProps> = (props) => {
-	const [local, others] = splitProps(props, [
-		"text",
-		"title",
-		"class",
-		"className",
-		"onCopy",
-		"children",
-		"dataTheme"
-	]);
+  const [local, others] = splitProps(props, [
+    "text",
+    "title",
+    "class",
+    "className",
+    "onCopy",
+    "children",
+    "dataTheme",
+    "copiedToken",
+  ]);
 
-	const [copied, setCopied] = createSignal(false);
+  const [copied, setCopied] = createSignal(false);
 
-	const copy = async () => {
-		await navigator.clipboard.writeText(local.text);
-		setCopied(true);
-		local.onCopy?.();
-		setTimeout(() => setCopied(false), 2000);
-	};
+  const copy = async () => {
+    await navigator.clipboard.writeText(local.text);
+    setCopied(true);
+    local.onCopy?.();
+    setTimeout(() => setCopied(false), 2000);
+  };
 
-	return (
-		<button
-			type="button"
-			onClick={copy}
-			title={local.title || "Copy to clipboard"}
-			data-theme={local.dataTheme}
-			class={twMerge(
-				"btn btn-sm btn-ghost",
-				copied() ? "text-success" : "",
-				local.class,
-				local.className
-			)}
-			{...others}
-		>
-			{local.children || (copied() ? "Copied!" : "Copy")}
-		</button>
-	);
+  const getContent = () => {
+    if (local.children) return local.children;
+    if (!copied()) return "Copy";
+
+    if (typeof local.copiedToken === "string") return local.copiedToken;
+    if (local.copiedToken) return <local.copiedToken />;
+    return "Copied!";
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={copy}
+      title={local.title || "Copy to clipboard"}
+      data-theme={local.dataTheme}
+      class={twMerge(
+        "btn btn-sm btn-ghost",
+        copied() ? "text-success" : "",
+        local.class,
+        local.className
+      )}
+      {...others}
+    >
+      {getContent()}
+    </button>
+  );
 };
 
-export default CopyButton; 
+export default CopyButton;
