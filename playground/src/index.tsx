29b3872873b7c4761d1c4d2daafab766b36d856e
Diego Martinez <diegoalejandromartinezrivas@gmail.com>
2026-04-07 03:32:08 -0400

    fix(playground): remove daisyui and restore CSS import order

--- playground/src/index.tsx
+++ playground/src/index.tsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-import App from "./App";
 import "./index.css";
+import App from "./App";
 
 document.documentElement.setAttribute("data-theme", "dark");
 
