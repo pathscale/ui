532fe0419ea156c4c09490b0debf2ab411472c31
Diego Martinez <diegoalejandromartinezrivas@gmail.com>
2026-04-10 18:21:02 -0400

    feat(streaming): add composable primitives and playground stress demo

---
+++ src/primitives/streaming/index.ts
@@ -0,0 +1,13 @@
+export {
+  useStreamingBuffer,
+  type StreamingBufferStrategy,
+  type UseStreamingBufferOptions,
+  type UseStreamingBufferResult,
+} from "./useStreamingBuffer";
+export {
+  useStreamingSubscription,
+  type StreamingSubscribeFn,
+  type StreamingSubscriptionObserver,
+  type UseStreamingSubscriptionOptions,
+  type UseStreamingSubscriptionResult,
+} from "./useStreamingSubscription";
