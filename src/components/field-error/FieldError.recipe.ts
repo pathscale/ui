99a1939bdccf7c0ea86b753c9d4f7ca3bf2c3f4e
meh <no-reply@pathscale.com>
2026-08-13 01:44:55 +0700

    refactor(field-error): port to Layouts

---
+++ src/components/field-error/FieldError.recipe.ts
@@ -0,0 +1,5 @@
+import { recipe } from "../../lib/layouts";
+export const CLASSES = {
+  base: "field-error",
+} as const;
+export const componentRecipe = recipe({component:"field-error",slots:{"field-error":{},"root":{},},});
