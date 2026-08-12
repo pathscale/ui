3dab0d5d30d5c0c72da4bce1a67406c816207954
meh <no-reply@pathscale.com>
2026-08-13 01:44:57 +0700

    refactor(tag): port to Layouts

--- src/components/tag/Tag.recipe.ts
+++ src/components/tag/Tag.recipe.ts
@@ -1,3 +1,4 @@
+import { recipe } from "../../lib/layouts";
 export const CLASSES = {
   Root: {
     base: "tag",
@@ -18,3 +19,4 @@ export const CLASSES = {
     removeButton: "tag__remove-button",
   },
 } as const;
+export const componentRecipe = recipe({component:"tag",slots:{"root":{},"tag":{},"tag-end-icon":{},"tag-remove-button":{},"tag-start-icon":{},},});
