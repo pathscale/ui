c1489bf194b8950783f929c83d56c6c2a9c062e9
meh <no-reply@pathscale.com>
2026-08-13 01:44:57 +0700

    refactor(radio-group): port to Layouts

--- src/components/radio-group/RadioGroup.recipe.ts
+++ src/components/radio-group/RadioGroup.recipe.ts
@@ -1,3 +1,4 @@
+import { recipe } from "../../lib/layouts";
 export const CLASSES = {
   base: "radio-group",
   slot: {
@@ -19,3 +20,4 @@ export const CLASSES = {
     invalid: "radio-group--invalid",
   },
 } as const;
+export const componentRecipe = recipe({component:"radio-group",slots:{"description":{},"error-message":{},"label":{},"radio-group":{},"radio-group-items":{},"root":{},},});
