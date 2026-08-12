f2a4da9484233c63b6d52af72a08a0160ae00c3c
meh <no-reply@pathscale.com>
2026-08-13 01:44:55 +0700

    refactor(error-message): port to Layouts

---
+++ src/components/error-message/ErrorMessage.recipe.ts
@@ -0,0 +1,5 @@
+import { recipe } from "../../lib/layouts";
+export const CLASSES = {
+  base: "error-message",
+} as const;
+export const componentRecipe = recipe({component:"error-message",slots:{"error-message":{},"root":{},},});
